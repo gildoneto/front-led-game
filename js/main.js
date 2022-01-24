7016e0aadd3a9754275be2a482f48f6778635dae
Gildo Neto <gildoneto@hotmail.com>
2022-01-24 15:19:11 -0300

    Implementa lógica do jogo

--- js/main.js
+++ js/main.js
@@ -14,16 +14,47 @@
 
 const endpoint = 'https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300'
 
-const getNumber = async() => {
-    const inputNumber = document.getElementById('input').value;
+let inputNumber = null
+let gameNumber = null
+
+let test = document.getElementById('test')
+let display = document.getElementById('display')
+
+const startGame = async() => {
+    inputNumber = 0
+    gameNumber = 0
+    display.innerText = '0'
+
     const data = await fetch(endpoint)
     if(data.status == 502){
-        console.log('erro 502')
+        display.innerText = 'ERRO 502'
     } else {
         const returnedNumber = await data.json()
-        console.log(returnedNumber.value)
+        gameNumber = returnedNumber.value
+        console.log(gameNumber)
     }
-    //const returnedNumber = await data.json()
-    //if ()
-    //console.log(returnedNumber.value)
+}
+
+const assignNumber = () => {
+    inputNumber = document.getElementById('input').value;
+}
+
+const displayNumber = () => {
+    display.innerText = inputNumber.toString()
+}
+
+const displayTest = (inputNumber, gameNumber) => {
+    if (inputNumber < gameNumber){
+        test.innerText = 'É menor'
+    } else if (inputNumber > gameNumber) {
+        test.innerText = 'É maior'
+    } else {
+        test.innerText = `Você acertou! ${gameNumber}`
+    }
+}
+
+const testNumber = () => {
+    assignNumber()
+    displayNumber()
+    displayTest(inputNumber, gameNumber)
 }
