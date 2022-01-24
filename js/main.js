6efd65ca9cc8e790960a617425b545cb7b4b6a5f
Gildo Neto <gildoneto@hotmail.com>
2022-01-24 12:22:46 -0300

    trata o status 502

--- js/main.js
+++ js/main.js
@@ -17,6 +17,13 @@ const endpoint = 'https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&ma
 const getNumber = async() => {
     const inputNumber = document.getElementById('input').value;
     const data = await fetch(endpoint)
-    const returnedNumber = await data.json()
-    console.log(returnedNumber)
+    if(data.status == 502){
+        console.log('erro 502')
+    } else {
+        const returnedNumber = await data.json()
+        console.log(returnedNumber.value)
+    }
+    //const returnedNumber = await data.json()
+    //if ()
+    //console.log(returnedNumber.value)
 }
